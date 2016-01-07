bfd410e2f53d6a2e48c2a8c0d5a9aa14610e5631
Thomas Muguet <t.muguet@thomasmuguet.info>
2016-01-07 01:42:22 +0100

    Added sanity checks & enhanced log messages

--- itunes-sync.py
+++ itunes-sync.py
@@ -35,7 +35,7 @@ music_destination = config.get('phone', 'music_destination')
 ssh_destination = config.get('phone', 'ssh_destination')
 
 
-logger.info("Loading library")
+logger.info("Loading library...")
 
 tree = ET.parse(os.path.expanduser(itunes_music_library))
 root = tree.getroot()
@@ -43,9 +43,10 @@ root = tree.getroot()
 tracks = {}
 playlists = []
 tracks_used = {}
+tracks_ignored = {}
 total_size = 0
 
-logger.info("Analyzing library")
+logger.info("Analyzing library...")
 
 # Find root of music folder
 for dicts in root.findall(".//dict[key='Music Folder']"):
@@ -57,93 +58,124 @@ for dicts in root.findall(".//dict[key='Music Folder']"):
             library_root = current.text.replace('file://', '')
         prev = current
 logger.debug("Found library root: " + library_root)
+if not os.path.isdir(library_root):
+    logger.error("Library root does not exist")
+    sys.exit(1)
 
 # Find all tracks in library
 for dicts in root.findall(".//dict[key='Location']"):
-    track_id = None
-    track_location = None
-    track_name = None
-    track_artist = None
-    track_album = None
-    track_size = 0
+    track = {
+        'name': "",
+        'artist': "",
+        'album': "",
+        'size': 0,
+    }
 
+    # Load data from XML
     it = dicts.iter()
     prev = it.next()
-
     for current in it:
         if prev.text == 'Track ID':
-            track_id = current.text
+            track['id'] = current.text
         elif prev.text == 'Name':
-            track_name = current.text
+            track['name'] = current.text
         elif prev.text == 'Artist':
-            track_artist = current.text
+            track['artist'] = current.text
         elif prev.text == 'Album':
-            track_album = current.text
+            track['album'] = current.text
         elif prev.text == 'Location':
-            track_location = current.text.replace('file://', '')
+            track['location_full_path'] = current.text.replace('file://', '')
         elif prev.text == 'Size':
-            track_size = int(current.text)
+            track['size'] = int(current.text)
         prev = current
 
-    if track_location.endswith('.ipa'):
-        # Ignore applications
+    # Sanity checks: all essential fields found?
+    if 'id' not in track and 'location_full_path' not in track:
+        logger.warning("Ignoring file: missing id and path (library corrupted?)")
+        logger.debug(track)
         continue
 
-    if not track_location.startswith(library_root):
-        logger.warning("Ignoring file outside library: #" + track_id + " at " + track_location)
+    if 'id' not in track:
+        logger.warning("Ignoring file at " + track['location_full_path'] + ": missing id (library corrupted?)")
+        logger.debug(track)
         continue
 
-    track_location = os.path.abspath(urllib.unquote(track_location))    # Location of file on this FS
-    track_location_relative = track_location.replace(library_root, '')  # Location of file relative to music folder
-    track_location_on_phone = music_destination + 'itunes-sync/' + track_location_relative  # Final location on the phone
+    if 'location_full_path' not in track:
+        logger.warning("Ignoring file #" + track['id'] + ": missing path (library corrupted?)")
+        logger.debug(track)
+        continue
 
-    tracks[track_id] = {
-        'location_full_path': track_location,
-        'location_relative': unicode(track_location_relative, 'utf-8'),
-        'location_on_phone': unicode(track_location_on_phone, 'utf-8'),
-        'name': track_name,
-        'artist': track_artist,
-        'album': track_album,
-        'size': track_size
-    }
+    # Sanity check: ignore applications
+    if track['location_full_path'].endswith('.ipa'):
+        tracks_ignored[track['id']] = track
+        continue
+
+    # Sanity check: ignore files that are not in a subdirectory of the library root
+    # This is a convenience to avoid finding a unique file path on the phone
+    if not track['location_full_path'].startswith(library_root):
+        logger.warning("Ignoring file #" + track['id'] + " at " + track['location_full_path'] + ": outside library")
+        logger.debug(track)
+        tracks_ignored[track['id']] = track
+        continue
+
+    # Compute some paths
+    track['location_full_path'] = os.path.abspath(urllib.unquote(track['location_full_path']))    # Location of file on this FS
+    track['location_relative'] = unicode(track['location_full_path'].replace(library_root, ''), 'utf-8')  # Location of file relative to music folder
+    track['location_on_phone'] = unicode(music_destination + 'itunes-sync/', 'utf-8') + track['location_relative']  # Final location on the phone
+
+    # Valid track: store its data
+    # File may not exist, that will be checked later if the track is included in a synchronized playlist
+    tracks[track['id']] = track
 
 logger.info("Found " + str(len(tracks)) + " tracks")
 
 # Find all playlists
 for dicts in root.findall(".//dict[key='Playlist ID']"):
-    playlist_name = None
-    playlist_tracks = []
+    playlist = {
+        'tracks': [],
+    }
 
+    # Load data from XML
     it = dicts.iter()
     prev = it.next()
-
     for current in it:
         if prev.text == 'Name':
-            playlist_name = current.text
+            playlist['name'] = current.text
             break
         prev = current
 
-    if not playlist_name in playlists_to_synchronize:
+    # Sanity checks: all essential fields found?
+    if 'name' not in playlist:
+        logger.debug("Ignoring playlist: missing name (library corrupted?)")
+        continue
+
+    # If we don't want to synchronize this playlist, don't bother parse its tracks
+    if playlist['name'] not in playlists_to_synchronize:
+        logger.debug("Found playlist " + playlist['name'] + " (ignored)")
         continue
 
     # Playlist to synchronize: get the tracks it contains
     for t in dicts.findall(".//dict[key='Track ID']/integer"):
         id = t.text
-        if not id in tracks:
-            logger.warning("Could not find track #" + id + " in library")
+        if id not in tracks:
+            if id in tracks_ignored:
+                logger.warning("Ignoring track #" + id + ": previously ignored (see previous warning)")
+                logger.debug(tracks_ignored[id])
+            else:
+                logger.warning("Ignoring track #" + id + ": not found in library (library corrupted?)")
             continue
 
-        playlist_tracks.append(t.text)
-        if not id in tracks_used:
+        playlist['tracks'].append(t.text)
+        if id not in tracks_used:
             tracks_used[id] = tracks[id]
             total_size += tracks[id]['size']
 
-    logger.info("Found playlist " + playlist_name + " (" + str(len(playlist_tracks)) + " tracks)")
-    playlists.append({'name': playlist_name, 'tracks': playlist_tracks})
+
+    logger.info("Found playlist " + playlist['name'] + " (" + str(len(playlist['tracks'])) + " tracks)")
+    playlists.append(playlist)
 
 
-
-logger.info("Preparing transfer of " + str(len(tracks_used)) + " tracks - " + str(total_size/(1024*1024)) + "MB")
+logger.info("Preparing transfer of " + str(len(tracks_used)) + " tracks - " + str(total_size/(1024*1024)) + "MB...")
 
 # Export data to send to the phone for creating playlists
 output = open('itunes-sync.pkl', 'wb')
@@ -162,14 +194,16 @@ for id,track in tracks_used.iteritems():
     full_filename = full_dir + "/" + filename
 
     if not os.path.exists(track['location_full_path']):
-        logger.warning("Could not find file " + track['location_full_path'])
+        logger.warning("Ignoring track #" + track['id'] + ": file " + track['location_full_path'] + " could not be found")
+        logger.debug(track)
         continue
 
     if not os.path.isdir(full_dir):
         os.makedirs(full_dir)
     if os.path.exists(full_filename):
-        # Can happen if a file is imported twice in iTunes
-        logger.warning("File already exists " + full_filename)
+        # Can happen if a file is imported twice in iTunes, or if library is corrupted
+        logger.warning("Ignoring track #" + track['id'] + ": file " + full_filename + " already exists (imported twice, or library corrupted?)")
+        logger.debug(track)
         continue
 
     os.symlink(track['location_full_path'], full_filename)
@@ -183,7 +217,7 @@ def call(args, log):
         line = s.stdout.readline()
         if not line:
             break
-        log.info(line.strip())
+        log.info("[" + args[0] + "] " + line.strip())
 
 
 logger.info("Transferring to " + ssh_destination + ":" + music_destination + "...")
